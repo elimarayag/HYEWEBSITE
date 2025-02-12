a8ab9019e113a00049332bd809ef94932a3e9b38
Ange <122997518+elimarayag@users.noreply.github.com>
2025-02-12 14:50:58 +0800

    updated

--- navbar.js
+++ navbar.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", function () {
             <ul class="nav-links">
                 <!-- Home Dropdown -->
                 <li class="dropdown">
-                    <a href="index.html">Home ▼</a>
+                    <a href="index.html">HOME ▼</a>
                     <div class="dropdown-content">
                         <a href="about.html">About Us</a>
                         <a href="partners.html">Partners</a>
@@ -19,59 +19,59 @@ document.addEventListener("DOMContentLoaded", function () {
 
                 <!-- Products Dropdown -->
                 <li class="dropdown">
-                    <a href="#">Products ▼</a>
+                    <a href="#">PRODUCTS ▼</a>
                     <div class="dropdown-content">
                         <div class="nested-dropdown">
                             <a href="#">HIKVISION ▶</a>
                             <div class="sub-dropdown">
-                                <a href="turbo-hd-tvi-720p.html">TURBO HD TVI 720P</a>
-                                <a href="turbo-hd-tvi-1080p.html">TURBO HD TVI 1080P</a>
-                                <a href="colorvu.html">COLORVU</a>
-                                <a href="hd1080p-audio-series.html">HD1080P AUDIO SERIES</a>
-                                <a href="iot-pir-series.html">IOT/PIR SERIES</a>
-                                <a href="ptz-speed-dome.html">PTZ SPEED DOME</a>
-                                <a href="acusense-dvr.html">ACUSENSE DVR</a>
-                                <a href="turbo-hd-dvr.html">TURBO HD DVR</a>
-                                <a href="hikvision-kit.html">HIKVISION KIT</a>
-                                <a href="hik-access-control.html">HIK ACCESS CONTROL</a>
+                                <a href="/hikvision/turbo-hd-tvi-720p.html">TURBO HD TVI 720P</a>
+                                <a href="/hikvison/turbo-hd-tvi-1080p.html">TURBO HD TVI 1080P</a>
+                                <a href="/hikvision/colorvu.html">COLORVU</a>
+                                <a href="/hikvision/hd1080p-audio-series.html">HD1080P AUDIO SERIES</a>
+                                <a href="/hikvision/iot-pir-series.html">IOT/PIR SERIES</a>
+                                <a href="/hikvision/ptz-speed-dome.html">PTZ SPEED DOME</a>
+                                <a href="/hikvision/acusense-dvr.html">ACUSENSE DVR</a>
+                                <a href="/hikvision/turbo-hd-dvr.html">TURBO HD DVR</a>
+                                <a href="/hikvision/hikvision-kit.html">HIKVISION KIT</a>
+                                <a href="/hikvision/hik-access-control.html">HIK ACCESS CONTROL</a>
                             </div>
                         </div>
                         <!-- AVTECH -->
                         <div class="nested-dropdown">
                             <a href="#">AVTECH ▶</a>
                             <div class="sub-dropdown">
-                                <a href="tvi-dome-camera.html">TVI DOME CAMERA</a>
-                                <a href="tvi-bullet-camera.html">TVI BULLET CAMERA</a>
-                                <a href="tvi-dvr-recorder.html">TVI DVR RECORDER</a>
-                                <a href="tvi-ptz-camera.html">TVI PTZ CAMERA</a>
+                                <a href="/avtech/tvi-dome-camera.html">TVI DOME CAMERA</a>
+                                <a href="/avtech/tvi-bullet-camera.html">TVI BULLET CAMERA</a>
+                                <a href="/avtech/tvi-dvr-recorder.html">TVI DVR RECORDER</a>
+                                <a href="/avtech/tvi-ptz-camera.html">TVI PTZ CAMERA</a>
                             </div>
                         </div>
                         <!-- DAHUA -->
                         <div class="nested-dropdown">
                             <a href="#">DAHUA ▶</a>
                             <div class="sub-dropdown">
-                                <a href="dome-camera.html">DOME CAMERA</a>
-                                <a href="bullet-camera.html">BULLET CAMERA</a>
-                                <a href="ptz-camera.html">PTZ CAMERA</a>
-                                <a href="nvr-recorder.html">NVR RECORDER</a>
-                                <a href="xvr-recorder.html">XVR RECORDER</a>
-                                <a href="networks.html">NETWORKS</a>
-                                <a href="pinhole-wireless.html">PINHOLE | WIRELESS</a>
+                                <a href="/dahua/dome-camera.html">DOME CAMERA</a>
+                                <a href="/dahua/bullet-camera.html">BULLET CAMERA</a>
+                                <a href="/dahua/ptz-camera.html">PTZ CAMERA</a>
+                                <a href="/dahua/nvr-recorder.html">NVR RECORDER</a>
+                                <a href="/dahua/xvr-recorder.html">XVR RECORDER</a>
+                                <a href="/dahua/networks.html">NETWORKS</a>
+                                <a href="/dahua/pinhole-wireless.html">PINHOLE | WIRELESS</a>
                             </div>
                         </div>
                         <!-- RUIJIE | NETWORKS -->
                         <div class="nested-dropdown">
                             <a href="#">RUIJIE | NETWORKS ▶</a>
                             <div class="sub-dropdown">
-                                <a href="reyee.html">REYEE</a>
-                                <a href="ruijie.html">RUIJIE</a>
-                                <a href="ruijie-kit.html">RUIJIE KIT</a>
-                                <a href="ruijie-management.html">RUIJIE MANAGEMENT</a>
-                                <a href="ruijie-accessories.html">RUIJIE ACCESSORIES</a>
+                                <a href="/ruijie/reyee.html">REYEE</a>
+                                <a href="/ruijie/ruijie.html">RUIJIE</a>
+                                <a href="/ruijie/ruijie-kit.html">RUIJIE KIT</a>
+                                <a href="/ruijie/ruijie-management.html">RUIJIE MANAGEMENT</a>
+                                <a href="/ruijie/ruijie-accessories.html">RUIJIE ACCESSORIES</a>
                                 <div class="nested-dropdown">
                             <a href="#">ZKTECO ▶</a>
                             <div class="sub-dropdown">
-                                <a href="biometrics-access-control.html">BIOMETRICS & ACCESS CONTROL</a>
+                                <a href="/ruijie/biometrics-access-control.html">BIOMETRICS & ACCESS CONTROL</a>
                             </div>
                             </div>
                             </div>
@@ -80,67 +80,67 @@ document.addEventListener("DOMContentLoaded", function () {
                         <div class="nested-dropdown">
                             <a href="#">ROSSLARE ▶</a>
                             <div class="sub-dropdown">
-                                <a href="#">ACCESS CONTROL SYSTEM</a>
+                                <a href="/rosslare/access-control.html">ACCESS CONTROL SYSTEM</a>
                             </div>
                         </div>
                         <!-- ITC AUDIO -->
                         <div class="nested-dropdown">
                             <a href="#">ITC AUDIO ▶</a>
                             <div class="sub-dropdown">
-                                <a href="#">ITC COMMERCIAL AUDIO</a>
+                                <a href="/itcaudio/itc-commercial.html">ITC COMMERCIAL AUDIO</a>
                             </div>
                         </div>
                         <!-- HONEYWELL -->
                         <div class="nested-dropdown">
                             <a href="#">HONEYWELL ▶</a>
                             <div class="sub-dropdown">
-                                <a href="#">EVACLITE</a>
-                                <a href="#">COMPACT PA</a>
-                                <a href="#">LOUD SPEAKER</a>
-                                <a href="#">FIRELITE - UL LISTED PANEL</a>
-                                <a href="#">MORLEY DXC - ADDRESSABLE</a>
-                                <a href="#">MORLEY LITE - ADDRESSABLE</a>
-                                <a href="#">MORLEY UL - ADDRESSABLE</a>
+                                <a href="/honeywell/evaclite.html">EVACLITE</a>
+                                <a href="/honeywell/compact.html">COMPACT PA</a>
+                                <a href="/honeywell/loudspeaker.html">LOUD SPEAKER</a>
+                                <a href="/honeywell/firelite.html">FIRELITE - UL LISTED PANEL</a>
+                                <a href="/honeywell/morleydxc.html">MORLEY DXC - ADDRESSABLE</a>
+                                <a href="/honeywell/morelylite.html">MORLEY LITE - ADDRESSABLE</a>
+                                <a href="/honeywell/morleyul.html">MORLEY UL - ADDRESSABLE</a>
                             </div>
                         </div>
                         <!-- iHORN -->
                         <div class="nested-dropdown">
                             <a href="#">iHORN ▶</a>
                             <div class="sub-dropdown">
-                                <a href="#">STANDALONE SMOKE DETECTOR</a>
-                                <a href="#">WIRED SMOKE | HEAT DETECTOR</a>
-                                <a href="#">BEAM DETECTOR | INFRARED HENCE</a>
-                                <a href="#">WIRED | WIRELESS GLASS DETECTOR</a>
-                                <a href="#">WIRELESS DETECTORS</a>
-                                <a href="#">WIRED PIR</a>
+                                <a href="/ihorn/standalone.html">STANDALONE SMOKE DETECTOR</a>
+                                <a href="/ihorn/wiredsmoke.html">WIRED SMOKE | HEAT DETECTOR</a>
+                                <a href="/ihorn/beamdetector.html">BEAM DETECTOR | INFRARED HENCE</a>
+                                <a href="/ihorn/wired.html">WIRED | WIRELESS GLASS DETECTOR</a>
+                                <a href="/ihorn/wireless.html">WIRELESS DETECTORS</a>
+                                <a href="/ihorn/wiredpir.html">WIRED PIR</a>
                             </div>
                         </div>
                         <!-- UBIQUITI -->
                         <div class="nested-dropdown">
                             <a href="#">UBIQUITI ▶</a>
                             <div class="sub-dropdown">
-                                <a href="#">Wireless Ubiquiti</a>
+                                <a href="/ubiquiti/wireless.html">Wireless Ubiquiti</a>
                             </div>
                         </div>
                         <!-- EZVIZ NETWORK -->
                         <div class="nested-dropdown">
                             <a href="#">EZVIZ NETWORK ▶</a>
                             <div class="sub-dropdown">
-                                <a href="#">EZVIZ</a>
+                                <a href="/ezviz/ezviz.html">EZVIZ</a>
                             </div>
                         </div>
                         <!-- GUARD PATROL -->
                         <div class="nested-dropdown">
                             <a href="#">GUARD PATROL ▶</a>
                             <div class="sub-dropdown">
-                                <a href="#">JWM High Tech</a>
+                                <a href="/guardpatrol/jwm.html">JWM High Tech</a>
                             </div>
                         </div>
                         <!-- HARD DRIVE -->
                         <div class="nested-dropdown">
                             <a href="#">HARD DRIVE ▶</a>
                             <div class="sub-dropdown">
-                                <a href="#">WD WESTERN DIGITAL</a>
+                                <a href="/harddrive/wd.html">WD WESTERN DIGITAL</a>
                             </div>
                         </div>
                     </div>
@@ -148,7 +148,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
                 <!-- News & Events Dropdown -->
                 <li class="dropdown">
-                    <a href="#">News & Events ▼</a>
+                    <a href="#">NEWS & EVENTS ▼</a>
                     <div class="dropdown-content">
                         <a href="honeywell-training.html">Honeywell Mid Segment FDAS & PAVA Basic Training & Product Updates</a>
                         <a href="ezviz-intro.html">EZVIZ - Product Introduction</a>
@@ -157,7 +157,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
                 <!-- Support Dropdown -->
                 <li class="dropdown">
-                    <a href="#">Support ▼</a>
+                    <a href="#">SUPPORT ▼</a>
                     <div class="dropdown-content">
                         <a href="videos.html">Videos</a>
                         <a href="downloads.html">Download</a>
@@ -165,8 +165,8 @@ document.addEventListener("DOMContentLoaded", function () {
                     </div>
                 </li>
 
-                <li><a href="blog.html">Blog</a></li>
-                <li><a href="contact.html">Contact Us</a></li>
+                <li><a href="blog.html">BLOG</a></li>
+                <li><a href="contact.html">CONTACT US</a></li>
             </ul>
         </nav>
     `;
